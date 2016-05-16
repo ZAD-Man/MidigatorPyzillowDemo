9c77387361f4915e9c93f16b75c46f3e7aab1c3a
Zach Durtschi <The.ZAD.One@GMail.com>
2016-05-16 06:27:28 -0600

    Fleshed out Zestimate; Start of Mongo

--- zillow.py
+++ zillow.py
@@ -1,8 +1,24 @@
 from pyzillow.pyzillow import ZillowWrapper, GetDeepSearchResults, GetUpdatedPropertyDetails, ZillowNoResults,\
     ZillowError, ZillowFail
+from pymongo.mongo_client import MongoClient
 
 
-def pull_property_data(search_address, search_zip, zillow_key):
+def connect_to_mongo(mongo_connection_string=None):
+    if mongo_connection_string is not None:
+        client = MongoClient(mongo_connection_string)
+    else:
+        client = MongoClient()
+    return client
+
+
+def get_zestimate_from_response(zestimate_response):
+    amount_location = 'response/zestimate/amount'
+    amount = zestimate_response.findtext(amount_location)
+    return amount
+
+
+def pull_property_data(search_address, search_zip, zillow_key, mongo_connection_string=None):
+    mongo_client = connect_to_mongo(mongo_connection_string)
     zillow_wrapper = ZillowWrapper(zillow_key)
     search_response = zillow_wrapper.get_deep_search_results(search_address, search_zip)
     search_results = GetDeepSearchResults(search_response)
@@ -10,9 +26,20 @@ def pull_property_data(search_address, search_zip, zillow_key):
     pass
 
 
-def get_zillow_estimated_price(zillow_id, zillow_key):
+def get_zestimate(zillow_id, zillow_key):
+    """
+    Uses Zillow's GetZestimate service to look up the Zestimate for a given property ID
+    :param zillow_id: ID of the property to look up
+    :param zillow_key: Zillow API key
+    :return: The property's Zestimate (str)
+    """
+    url = 'http://www.zillow.com/webservice/GetZestimate.htm'
+    params = {
+        'zpid': zillow_id,
+        'zws-id': zillow_key,
+    }
+
     zillow_wrapper = ZillowWrapper(zillow_key)
-    property_response = zillow_wrapper.get_updated_property_details(zillow_id)
-    property_results = GetUpdatedPropertyDetails(property_response)
-    # TODO: pyzillow doesn't get the price - write my own
-    pass
+    zestimate_response = zillow_wrapper.get_data(url, params)
+    zestimate = get_zestimate_from_response(zestimate_response)
+    return zestimate
