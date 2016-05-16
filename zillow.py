8b3d7628fc7a119871223ed13ed3297e8c8ec327
Zach Durtschi <The.ZAD.One@GMail.com>
2016-05-16 08:02:27 -0600

    Flesh out pull_property_data, add update option

--- zillow.py
+++ zillow.py
@@ -17,13 +17,49 @@ def get_zestimate_from_response(zestimate_response):
     return amount
 
 
-def pull_property_data(search_address, search_zip, zillow_key, mongo_connection_string=None):
+def pull_property_data(search_address, search_zip, zillow_key, mongo_connection_string=None, update=False):
+    """
+    Gets property data for given address/zip and writes it to given mongo database
+    Will write to zillow.property_data in mongo
+    :param search_address: Address to search for
+    :param search_zip: ZIP code to search for
+    :param zillow_key: Zillow API key
+    :param mongo_connection_string: String with mongo credentials URI, e.g. mongodb://uname:pass@host:port
+                                    See https://docs.mongodb.com/v3.0/reference/connection-string/
+    :param update: If True, and a document with found zillow_id already exists, updates that document, otherwise ignores
+    :return: Mongo info if inserted/updated, else None
+    """
     mongo_client = connect_to_mongo(mongo_connection_string)
     zillow_wrapper = ZillowWrapper(zillow_key)
     search_response = zillow_wrapper.get_deep_search_results(search_address, search_zip)
     search_results = GetDeepSearchResults(search_response)
-    # TODO: Store results in Mongo
-    pass
+    result_data = vars(search_results)
+
+    # We don't want the xml data in there
+    try:
+        del result_data['data']
+    except KeyError:
+        pass
+
+    zillow_db = mongo_client.zillow
+    query = {
+        'zillow_id': result_data['zillow_id']
+    }
+
+    mongo_info = None
+    if update:
+        update = {
+            '$set': result_data,
+        }
+        options = {
+            'upsert': True,
+        }
+        mongo_info = zillow_db.property_data.update(query, update, options)
+    else:
+        if zillow_db.property_data.find(query).count() == 0:
+            mongo_info = zillow_db.property_data.insert(result_data)
+
+    return mongo_info
 
 
 def get_zestimate(zillow_id, zillow_key):
